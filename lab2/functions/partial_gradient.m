fa3cbbf49a50a17acdcaefbe918d90edfcc07290
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-02 14:11:48 +0100

    defined process_epoch functions and updated partial_gradient and lab2

--- lab2/functions/partial_gradient.m
+++ lab2/functions/partial_gradient.m
@@ -2,7 +2,7 @@ function  [wgrad, w0grad] = partial_gradient(w, w0, example_train, label_train)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-y = example_train*w + w0;
+y = dot(example_train,w) + w0;
 
 p = exp(y)/(1+exp(y));
 
---
+++ lab2/functions/process_epoch.m
@@ -0,0 +1,15 @@
+function [w, w0] = process_epoch(w, w0, lrate, examples_train, labels_train)
+%PROCESS_EPOCH Summary of this function goes here
+%   Detailed explanation goes here
+
+idx = randperm(size(examples_train,2));
+
+for i=idx
+    [wgrad,w0grad] = partial_gradient(w,w0,cell2mat(examples_train(i)), labels_train(i));
+
+    w = w - lrate*wgrad;
+
+    w0 = w0 - lrate*w0grad;
+
+end
+
