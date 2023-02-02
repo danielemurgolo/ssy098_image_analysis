e9c255640f66976b9ecabedc51c3219c66645f39
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-02 11:58:08 +0100

    created partial_gradient function

---
+++ lab2/functions/partial_gradient.m
@@ -0,0 +1,21 @@
+function  [wgrad, w0grad] = partial_gradient(w, w0, example_train, label_train)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+y = example_train*w + w0;
+
+p = exp(y)/(1+exp(y));
+
+if label_train == 1
+
+    wgrad = (p-1) * example_train;
+    w0grad = p-1;
+
+else
+
+    wgrad = p * example_train;
+    w0grad = p;
+
+end
+
+end
