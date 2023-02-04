baa8543c5a7307da1e17a8177f99c0fbfd395c1a
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-04 01:21:53 +0100

    updated gaussian_gradients

--- lab1/functions/gaussian_gradients.m
+++ lab1/functions/gaussian_gradients.m
@@ -2,7 +2,7 @@ function [grad_x,grad_y] = gaussian_gradients(image,std)
 %GAUSSIAN_GRADIENTS Summary of this function goes here
 %   Detailed explanation goes here
 filtered = gaussian_filter(image, std);
-grad_x = imfilter(filtered, [-0.5; 0; 0.5], 'conv');
-grad_y = imfilter(filtered, [-0.5, 0, 0.5], 'conv');
+grad_x = imfilter(filtered, [-0.5, 0, 0.5], 'conv');
+grad_y = imfilter(filtered, [-0.5; 0; 0.5], 'conv');
 end
 
