91db9d17a588423f971235440033f01c0e267608
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-06 17:23:20 +0100

    create train_classifier function

---
+++ lab2/functions/train_classifier.m
@@ -0,0 +1,10 @@
+function net = train_classifier(layers, imgs_train, labels_train, imgs_val, labels_val)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+disp('Training the network')
+options = trainingOptions('sgdm', 'ExecutionEnvironment','gpu', 'Verbose',false);
+net = trainNetwork(imgs_train,labels_train, layers, options);
+tp = nnz(net.classify(imgs_val) == labels_val); 
+accuracy = tp / length(labels_val);
+disp(['The network achieved an accuracy of: ' num2str((accuracy)*100),'%'])
+end
