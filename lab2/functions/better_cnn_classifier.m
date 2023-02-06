87e396b2c9c096a1a56705a0426bc33fa3268fb2
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-06 18:43:41 +0100

    create better_cnn_classifier function, completed code in lab2 and modified n_epoch in train_classifier

---
+++ lab2/functions/better_cnn_classifier.m
@@ -0,0 +1,17 @@
+function layers = better_cnn_classifier()
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+layers = [
+    imageInputLayer([28,28,1]);
+    convolution2dLayer(3,25);
+    reluLayer();
+    maxPooling2dLayer(2, 'Stride',2);
+    convolution2dLayer(5,20);
+    reluLayer();
+    maxPooling2dLayer(2, 'Stride',2);
+    fullyConnectedLayer(50);
+    fullyConnectedLayer(10);
+    softmaxLayer();
+    classificationLayer()
+];
+end
--- lab2/functions/train_classifier.m
+++ lab2/functions/train_classifier.m
@@ -1,8 +1,9 @@
 function net = train_classifier(layers, imgs_train, labels_train, imgs_val, labels_val)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+epochs = 25;
 disp('Training the network')
-options = trainingOptions('sgdm', 'ExecutionEnvironment','gpu', 'Verbose',false);
+options = trainingOptions('sgdm', 'ExecutionEnvironment','gpu','MaxEpochs',epochs, 'Verbose',false);
 net = trainNetwork(imgs_train,labels_train, layers, options);
 tp = nnz(net.classify(imgs_val) == labels_val); 
 accuracy = tp / length(labels_val);
