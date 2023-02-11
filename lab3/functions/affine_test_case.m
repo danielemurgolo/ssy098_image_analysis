6c59942e84121b94a8c6379beb6bf03d8b9f1c56
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-11 19:58:48 +0100

    updated affine_test_case because of a bug

--- lab3/functions/affine_test_case.m
+++ lab3/functions/affine_test_case.m
@@ -3,7 +3,7 @@ function [pts, pts_tilde, A_true, t_true] = affine_test_case()
 %and applies to them a random transformation
 N = randi([3,200]);
 A_true = rand(2,2);
-t_true = randi(2,1)*100;
+t_true = rand(2,1)*100;
 pts = rand(2, N);
 pts_tilde = A_true*pts + t_true;
 end
