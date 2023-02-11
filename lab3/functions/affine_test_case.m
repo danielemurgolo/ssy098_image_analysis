6583ac5d3594df4b3f9db57fc22b68df7f4e3101
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-11 19:03:20 +0100

    added description to affine_test_case

--- lab3/functions/affine_test_case.m
+++ lab3/functions/affine_test_case.m
@@ -1,6 +1,6 @@
 function [pts, pts_tilde, A_true, t_true] = affine_test_case()
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%The function generate N (between 3 and 200) points randomly
+%and applies to them a random transformation
 N = randi([3,200]);
 A_true = rand(2,2);
 t_true = randi(2,1)*100;
