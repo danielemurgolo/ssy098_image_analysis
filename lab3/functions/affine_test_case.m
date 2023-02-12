33eb9ed3eeb36243ed7b0396ea1b95a78165c824
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-12 15:48:26 +0100

    updated affine_test_case since it was not allowing negative values in A matrix

--- lab3/functions/affine_test_case.m
+++ lab3/functions/affine_test_case.m
@@ -2,8 +2,8 @@ function [pts, pts_tilde, A_true, t_true] = affine_test_case()
 %The function generate N (between 3 and 200) points randomly
 %and applies to them a random transformation
 N = randi([3,200]);
-A_true = rand(2,2);
-t_true = rand(2,1)*100;
+A_true = randn(2,2);
+t_true = randn(2,1)*100;
 pts = rand(2, N);
 pts_tilde = A_true*pts + t_true;
 end
