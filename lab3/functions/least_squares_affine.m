2a7cf45abcc7d137fa112bdcb1e604022ebe2e3b
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-14 16:31:06 +0100

    created least_squares_affine function

---
+++ lab3/functions/least_squares_affine.m
@@ -0,0 +1,5 @@
+function [A, t] = least_squares_affine(pts, pts_tilde)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+[A,t] = estimate_affine(pts, pts_tilde);
+end
