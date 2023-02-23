4d6aa1c99e3ca621853e9a589c92a66780154fac
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-23 15:31:46 +0100

    created script triangulate_sequence

---
+++ lab4/triangulate_sequence.m
@@ -0,0 +1,16 @@
+load sequence.mat
+threshold = 5;
+Us = [];
+N_examples = length(triangulation_examples);
+for i=1:N_examples
+
+    Ps = triangulation_examples(i).Ps;
+    xs = triangulation_examples(i).xs;
+    
+    [U, nbr_inliers] = ransac_triangulation(Ps, xs, threshold);
+
+    if nbr_inliers >=2
+        Us = [Us U];
+    end
+
+end
