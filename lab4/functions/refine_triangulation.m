a02c5ae4d7f7f103e3194e8cde5b0ac01f24f2f6
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-26 11:34:34 +0100

    created refinetriangulaion function

---
+++ lab4/functions/refine_triangulation.m
@@ -0,0 +1,15 @@
+function U = refine_triangulation(Ps, us, Uhat)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+    n_iter = 5;
+    
+    for i=1:n_iter
+
+        r = compute_residuals(Ps,us,Uhat);
+        J = compute_jacobian(Ps,Uhat);
+        U = U - ((J'*J)\J)*r;
+
+    end
+
+end
