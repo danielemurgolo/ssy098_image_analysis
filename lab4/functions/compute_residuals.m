897c1b09cc30088d8cb3823f9c380296da5b3571
danielemurgolo <daniele.murgolo@protonmail.com>
2023-02-26 11:05:43 +0100

    created compute_residuals function

---
+++ lab4/functions/compute_residuals.m
@@ -0,0 +1,32 @@
+function all_residuals = compute_residuals(Ps, us, U);
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    N_cameras = length(Ps);
+    Us = [U;1];
+    all_residuals = zeros(2*N_cameras,1);
+    
+    for i=1:2:2*N_cameras
+  
+        P = Ps{round(i/2)};
+        a = P(1,:);
+        b = P(2,:);
+        c = P(3,:);
+        positive = check_depths(Ps, U);
+
+        if positive
+
+            x = (a*Us)/(c*Us) - us(1,round(i/2));
+            y = (b*Us)/(c*Us) - us(2,round(i/2));
+            all_residuals(i) = x;
+            all_residuals(i+1) = y;
+            
+        else
+
+            all_residuals(i) = inf;
+            all_residuals(i+1) = inf;
+
+        end
+
+    end
+
+end
