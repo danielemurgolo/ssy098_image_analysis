5e0b7a4e2ea25ca883cf9c3ba6f5efd9fa318886
danielemurgolo <daniele.murgolo@protonmail.com>
2023-03-06 21:59:26 +0100

    added header comment and formatted code

--- project/functions/permute_dataset.m
+++ project/functions/permute_dataset.m
@@ -1,8 +1,23 @@
-function [data,labels] = permute_dataset(data,labels)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [permutedImageData, categoricalLabels] = permute_dataset(data, labels)
+% Function Name: permute_dataset
+%
+% Description: This function reshapes the input data so that the last
+%              dimension of the output shape corresponds to the index.
+%              Additionally, it converts the input labels to categorical format.
+%
+% Inputs:
+%   - data: matrix of size N x 3072
+%   - labels: array of length N
+%
+% Outputs:
+%   - permutedImageData: matrix of size 32 x 32 x 3 x N
+%   - categoricalLabels: categorical array of length N
+%
+% Author: Daniele Murgolo
+% Date: March 6th, 2023
+
 N = size(data, 1);
 data = data';
-data = reshape(data, [32,32,3,N]);
-labels = categorical(labels);
+permutedImageData = reshape(data, [32, 32, 3, N]);
+categoricalLabels = categorical(labels);
 end
